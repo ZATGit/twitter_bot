55ae7a8979ffd9e46d6bd0c9d15c1222fb9f23d4
ZATGit <zachtrembly@gmail.com>
2019-04-24 13:22:15 -0400

    Add scrape feature to inspect & fetch content.

--- bot.py
+++ bot.py
@@ -19,3 +19,20 @@ oauth = OAuth(
     credentials.CONSUMER_SECRET
 )
 t = Twitter(auth=oauth)
+
+
+
+def npr_scrape():
+    #recognized as real client
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)'
+            'AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'
+    }
+    #fetch & save page info to r
+    r = requests.get('https://www.npr.org/sections/science/', headers=HEADERS)
+    #r.content 'inspect element' source html, pass to fromstring (lxml library)
+    tree = fromstring(r.content)
+    links = tree.xpath('//div[@class="respArchListImg"]//div[@class="title"]')
+    print(links)
+
+npr_scrape()
