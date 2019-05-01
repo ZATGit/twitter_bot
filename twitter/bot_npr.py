2e074cb17229a23e0eb25b38632da05de76c6726
ZATGit <zachtrembly@gmail.com>
2019-05-01 17:29:21 -0400

    Create 'extract_text_paragraph' & 'extract_sentence' functions so 'npr_scrape' functionality can be used on multiple functions and avoid redundancy. Move global variable and import commands to main for same reason.

--- twitter/bot_npr.py
+++ twitter/bot_npr.py
@@ -1,64 +1,24 @@
-import time
-import random
-
-from lxml.html import fromstring
-import nltk
-#dl dataset for parse paragraphs & tokenize
-nltk.download('punkt')
-import requests
-from twitter import OAuth, Twitter
-
-import credentials
-
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-
-oauth = OAuth(
-    credentials.ACCESS_TOKEN,
-    credentials.ACCESS_SECRET,
-    credentials.CONSUMER_KEY,
-    credentials.CONSUMER_SECRET
-)
-t = Twitter(auth=oauth)
-
-
-
 def npr_scrape():
     """Scrape articles from npr.org"""
-    #recognized as real client
-    HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)'
-            'AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'
-        }
+
     #fetch & save page info to r
-    r = requests.get('https://npr.org', headers=HEADERS)
+    url = 'https://npr.org'
+    r = requests.get(url, headers=HEADERS)
     #r.content 'inspect element' source html, pass to fromstring (lxml library)
     tree = fromstring(r.content)
     links = tree.xpath('//section[@class="featured-group"]//div[@class="story-text"]/a/@href')
     print(links)
 
-    #fetch paragraphs
+    #fetch paragraphs and extract text
     for link in links:
         r = requests.get(link, headers=HEADERS)
         page_tree = fromstring(r.content)
         paragraphs = page_tree.xpath('//div[@class="storytext"]/p')
-        paragraphs_text = [paragraphs.text_content() for paragraph in paragraphs if paragraph.text_content()]
-        paragraph = random.choice(paragraphs_text)
-        paragraph_tokenized = tokenizer.tokenize(paragraph)
+        paragraph = extract_text_paragraph(paragraphs)
+        text = extract_text(paragraph)
 
-        #sort text from random paragraph. skip to next article if no matching sentence in 10 attempts.
-        for _ in range(10):
-            story_text = random.choice(paragraph_tokenized)
-            if text and 160 < len(text) < 280:
-                break
+        if not text:
+            continue
 
-        elif:
-
-    else:
-
-        yield None
-
-
-
-
-
-npr_scrape()
+        #yield instead of return to avoid always getting first article in list and fewer requests
+        yield '"%s" %s' % (text, link)
--- twitter/main.py
+++ twitter/main.py
@@ -1,7 +1,57 @@
+import time
+import random
+
+from lxml.html import fromstring
+import nltk
+#dl dataset for parse paragraphs & tokenize
+nltk.download('punkt')
+import requests
+from twitter import OAuth, Twitter
+
+import credentials
+
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+oauth = OAuth(
+    credentials.ACCESS_TOKEN,
+    credentials.ACCESS_SECRET,
+    credentials.CONSUMER_KEY,
+    credentials.CONSUMER_SECRET
+)
+t = Twitter(auth=oauth)
+
+from bot_npr import *
+
+#recognized as real client
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)'
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'
+}
+
+def extract_text_paragraph(paragraphs):
+    """Extracts text from paragraph elements and returns random tokenized paragraph."""
+    paragraphs = [paragraph.text_content() for paragraph in paragraphs if paragraph.text_content()]
+    paragraph = random.choice(paragraphs)
+
+    return tokenizer.tokenize(paragraph)
+
+def extract_sentence(paragraph):
+    """Returns random text if correct length from a paragraph."""
+
+    # sort text from random paragraph.
+    for _ in range(10):
+        text = random.choice(paragraph)
+        if text and 180 < len(text) < 280:
+            return text
+
+    #skip to next article if no matching sentence in 10 attempts.
+    return None
+
+
+
 def main():
 
-    while
+    npr_scrape()
 
-        for
-
-            for
+if __name__ == "__main__":
+    main()
